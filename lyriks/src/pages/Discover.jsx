165565fbf4a0b105388b2b7d904de1819951ffc3
Qwerty@126 <indrmy.126@gmail.com>
2025-05-14 14:03:21 +0530

    s

--- lyriks/src/pages/Discover.jsx
+++ lyriks/src/pages/Discover.jsx
@@ -1,14 +1,27 @@
-import { Error, Loader, SongCard } from "../components";
-import { genres } from "../assets/constants"
-
-
+/* eslint-disable indent */
+/* eslint-disable no-console */
+/* eslint-disable no-trailing-spaces */
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
 
 const Discover = () => {
-    console.log(genres)
-    return()
-    <div>Discover</div>;
+    const genrestitles = 'Pop';
 
-}
-
+    return (
+      <div className=" flex flex-col">
+        <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+          <h2 className="text-white font-bold text-3xl text-left">Discover {genrestitles}</h2>
+          <select
+            onChange={() => { }}
+            value=""
+            className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+          >
+            {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+          </select>
+        </div>
+        
+      </div>
+    );
+};
 
 export default Discover;
