431eac1ec865e056f6572402807b291d61e76fb7
Qwerty@126 <indrmy.126@gmail.com>
2025-05-22 16:17:02 +0530

    s

--- lyriks/src/redux/services/shazamCore.js
+++ lyriks/src/redux/services/shazamCore.js
@@ -3,10 +3,10 @@ const options = {
   headers: {
     'x-rapidapi-key': 'f8a723122fmshdc5c3d67040495fp125fd1jsn1dc864ba80be',
     'x-rapidapi-host': 'shazam-core.p.rapidapi.com'
-  }
+  },
 };
 
 fetch('https://shazam-core.p.rapidapi.com/v1/charts/world?country_code=DZ', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
