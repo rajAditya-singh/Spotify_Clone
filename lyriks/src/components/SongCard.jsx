b08fb358201e25f9952ac22381519c0d4883689c
Qwerty@126 <indrmy.126@gmail.com>
2025-05-27 17:57:50 +0530

    s

--- lyriks/src/components/SongCard.jsx
+++ lyriks/src/components/SongCard.jsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 const SongCard = () => (
   <div>SongCard</div>
 );
