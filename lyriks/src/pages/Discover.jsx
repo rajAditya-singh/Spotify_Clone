7e9506ffd8eab5a7b151ef7ff64ea7744db29a95
Qwerty@126 <indrmy.126@gmail.com>
2025-05-27 17:47:46 +0530

    s

--- lyriks/src/pages/Discover.jsx
+++ lyriks/src/pages/Discover.jsx
@@ -10,8 +10,9 @@ const Discover = () => {
     const { data, isFetching, error } = useGetTopChartQuery();
     const genrestitles = 'Pop';
 
-    console.log(data)
-
+    if (isFetching) return <Loader title="Loading Songs..." />;
+    if (error) return <Error />;
+    
     return (
       <div className=" flex flex-col">
         <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -26,7 +27,7 @@ const Discover = () => {
         </div>
         
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+          {data?.map((song, i) => (
             <SongCard
               key={song}
               song={song}
