ab89c2e89c8303895b1baaa79bd085035a94d13a
Qwerty@126 <indrmy.126@gmail.com>
2025-05-28 18:01:05 +0530

    s

--- lyriks/src/redux/services/shazamCore.js
+++ lyriks/src/redux/services/shazamCore.js
@@ -1,18 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'x-rapidapi-key': 'f8a723122fmshdc5c3d67040495fp125fd1jsn1dc864ba80be',
-    'x-rapidapi-host': 'shazam-core.p.rapidapi.com',
-  },
-};
-
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world?country_code=DZ', options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
