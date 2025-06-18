3a542a758f6ce5f9f97fddc5473d45ccf34a7d8b
Qwerty@126 <indrmy.126@gmail.com>
2025-06-18 18:07:14 +0530

    s

--- lyriks/src/components/SongCard.jsx
+++ lyriks/src/components/SongCard.jsx
@@ -4,27 +4,28 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import PlayPause from "./PlayPause";
+import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, i }) => {
-  const dispatch = useDispatch();
   const activeSong = 'Test';
 
-  const { attributes } = song;
-  if (!attributes) return null;
-
-  const { name, artistName, artwork } = attributes;
+  const handlePauseClick = () => {};
+  const handlePlayClick = () => {};
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
-        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === name ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-          <PlayPause />
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.attributes?.name ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+          <PlayPause
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img
           alt="song_img"
-          src={artwork?.url?.replace('{w}x{h}', '250x250') || 'https://via.placeholder.com/250'}
+          src={song.attributes?.artwork?.url?.replace('{w}x{h}', '250x250') || 'https://via.placeholder.com/250'}
           className="w-full h-full rounded-lg"
         />
       </div>
